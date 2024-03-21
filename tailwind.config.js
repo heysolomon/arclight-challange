4d6ec4f359c9a256b6a2dc3aca9c459aaac25fb0
Solomon Akuson <akusonsolomon15@gmail.com>
2024-03-21 23:27:53 +0100

    new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#242DFE",
+        secondary: {
+          DEFAULT: "#595959",
+          light: "#9FA5B6",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
